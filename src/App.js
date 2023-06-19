b128a7bdb67f304a7ed78b01dffa71eb40a09f21
Gabriel Soares <gabrsoares@outlook.com>
2023-06-19 17:06:24 -0300

    reconvert to class component & add keyboard function & set max number of characters

--- src/App.js
+++ src/App.js
@@ -1,89 +1,147 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './App.css';
 import Button from './Components/Button/Button';
 import Display from './Components/Display/Display';
 
-const App = () => {
-  const [displayNumber, setDisplayNumber] = useState(0);
-  const [prevDisplay, setPrevDisplay] = useState('');
-  const [prevNumber, setPrevNumber] = useState('');
-  const [operation, setOperation] = useState('');
-  const [didOperated, setDidOperated] = useState(false);
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    
+    this.state = {
+      displayNumber: 0,
+      prevDisplay: "",
+      prevNumber: "",
+      operation: "",
+      didOperated: false
+    }
+  } 
 
-  const handleInsertNumber = (value) => {
-    setDisplayNumber(didOperated ? value : displayNumber === 0 ? value : displayNumber + value); //primeiro checa se ja foi feita alguma operação, para não concatenar os valores, depois checa se o valor é 0, para trocá-lo pelo número
-    setDidOperated(false);
-  };
+  componentDidMount() {
+    document.addEventListener("keydown", this.handleInsertKey)
+  }
 
-  const handleClear = () => {
-    setDisplayNumber(0);
-    setPrevDisplay('');
-    setPrevNumber('');
-    setOperation('');
-    setDidOperated(false);
-  };
+  componentWillUnmount() {
+    document.removeEventListener("keydown", this.handleInsertKey)
+  }
 
-  const handleInsertOperation = (value) => {
-    setPrevNumber(displayNumber);
-    setPrevDisplay(displayNumber + value);
-    setOperation(value);
-    setDidOperated(true); // serve para limpar o display ao inserir mais números após a operação
-  };
+  handleInsertKey = (event) => {
+    const key = event.key
+    const windowSize = window.innerWidth >= 1200? 18: window.innerWidth >= 600? 14 : window.innerWidth >=400? 9 : 7 //pega o tamanho da tela para definir o numero máximo de caracteres na calculadura
+    if (!isNaN(key)){ //validação feita para não inserir outros valores além de números
+      if(this.state.displayNumber.length > windowSize && !this.state.didOperated){
+        event.preventDefault()
+      } else {
+        this.setState({
+          displayNumber: this.state.didOperated? key : this.state.displayNumber === 0? key: this.state.displayNumber + key, // primeiro checa se ja foi feita alguma operação, para não concatenar os valores, depois checa se o valor é 0, para trocá-lo pelo número
+          didOperated: false
+        })
+      }
+    } 
+    else if (key.match(/[+\-*/]/)) {
+      this.handleInsertOperation(key)
+    }
+    else if (key === "Escape" || key === "Backspace"){
+      this.handleClear()
+    }
+    else if(key === "Enter" || key === "=") {
+      this.handleEndOperation()
+    }
+  }
 
-  const handleEndOperation = () => {
-    if (prevNumber === '' && operation === '') {
-      setDisplayNumber(displayNumber);
-    } else {
-      switch (operation) {
-        case '/':
-          setDisplayNumber(parseFloat(prevNumber / displayNumber));
-          break;
-        case 'x':
-          setDisplayNumber(parseFloat(prevNumber * displayNumber));
-          break;
-        case '-':
-          setDisplayNumber(parseFloat(prevNumber - displayNumber));
-          break;
-        case '+':
-          setDisplayNumber(parseFloat(prevNumber) + parseFloat(displayNumber));
-          break;
-        default:
-          setDisplayNumber(prevNumber);
+  handleInsertNumber = (value) => {
+    const windowSize = window.innerWidth >= 1200? 18: window.innerWidth >= 600? 14 : window.innerWidth >=400? 9 : 7
+    if(!(this.state.displayNumber.length > windowSize && !this.state.didOperated)){
+      this.setState({
+        displayNumber: this.state.didOperated? value : this.state.displayNumber === 0? value: this.state.displayNumber + value, // primeiro checa se ja foi feita alguma operação, para não concatenar os valores, depois checa se o valor é 0, para trocá-lo pelo número
+        didOperated: false
+      })
+    }
+  }
+
+  handleClear () {
+    this.setState({
+      displayNumber: 0,
+      prevDisplay: "",
+      prevNumber: "",
+      operation: "",
+      didOperated: false
+    })
+  }
+
+  handleInsertOperation = (value) => {
+    this.setState({
+      prevNumber: this.state.displayNumber,
+      prevDisplay: this.state.displayNumber + value,
+      operation: value,
+      didOperated: true // serve para limpar o display ao inserir mais números após a operação
+    })
+  }
+
+  handleEndOperation = () => {
+    if (this.state.prevNumber === "" && this.state.operation === "") { // retorna
+      this.setState({
+        displayNumber: this.state.displayNumber
+      })
+    }
+    else {
+      switch (this.state.operation) {
+        case '/': this.setState({ 
+          displayNumber: parseFloat(this.state.prevNumber / this.state.displayNumber)
+        })
+        break;
+        case '*': this.setState({ 
+          displayNumber: parseFloat(this.state.prevNumber * this.state.displayNumber)
+        })
+        break;
+        case '-': this.setState({ 
+          displayNumber: parseFloat(this.state.prevNumber - this.state.displayNumber)
+        })
+        break;
+        case '+': this.setState({ 
+          displayNumber: parseFloat(this.state.prevNumber) + parseFloat(this.state.displayNumber)
+        })
+        break;
+        default: this.setState({displayNumber: this.state.prevNumber})
       }
     }
+    
+    this.setState({
+      prevDisplay: "",
+      didOperated: true,
+      prevNumber: ""
+    })
+  }
 
-    setPrevDisplay('');
-    setDidOperated(true);
-    setPrevNumber('');
-  };
-
-  return (
-    <div className="App">
-      <Display previous={prevDisplay} atual={displayNumber} />
-      <div className="buttonList">
-        <Button value="" />
-        <Button value="" />
-        <Button onClick={handleClear} value="C" />
-        <Button onClick={() => handleInsertOperation('/')} value="/" />
-        <Button onClick={() => handleInsertNumber('7')} value="7" />
-        <Button onClick={() => handleInsertNumber('8')} value="8" />
-        <Button onClick={() => handleInsertNumber('9')} value="9" />
-        <Button onClick={() => handleInsertOperation('x')} value="x" />
-        <Button onClick={() => handleInsertNumber('4')} value="4" />
-        <Button onClick={() => handleInsertNumber('5')} value="5" />
-        <Button onClick={() => handleInsertNumber('6')} value="6" />
-        <Button onClick={() => handleInsertOperation('-')} value="-" />
-        <Button onClick={() => handleInsertNumber('1')} value="1" />
-        <Button onClick={() => handleInsertNumber('2')} value="2" />
-        <Button onClick={() => handleInsertNumber('3')} value="3" />
-        <Button onClick={() => handleInsertOperation('+')} value="+" />
-        <Button value="" />
-        <Button onClick={() => handleInsertNumber('0')} value="0" />
-        <Button value="" />
-        <Button onClick={handleEndOperation} value="=" />
+  render() {
+      return (
+      <div className="App">
+        <Display previous={this.state.prevDisplay} atual={this.state.displayNumber}/>
+        <div className="buttonList">
+          <Button value =""/>
+          <Button value =""/>
+          <Button onClick ={this.handleClear.bind(this)} value ="C"/>
+          <Button onClick ={() => this.handleInsertOperation("/")} value ="/"/>
+          <Button onClick={() => this.handleInsertNumber("7")} value="7" />
+          <Button onClick={() => this.handleInsertNumber("8")} value="8" />
+          <Button onClick={() => this.handleInsertNumber("9")} value="9" />
+          <Button onClick ={() => this.handleInsertOperation("*")} value ="x"/>
+          <Button onClick={() => this.handleInsertNumber("4")} value ="4"/>
+          <Button onClick={() => this.handleInsertNumber("5")} value ="5"/>
+          <Button onClick={() => this.handleInsertNumber("6")} value ="6"/>
+          <Button onClick ={() => this.handleInsertOperation("-")} value ="-"/>
+          <Button onClick={() => this.handleInsertNumber("1")} value ="1"/>
+          <Button onClick={() => this.handleInsertNumber("2")} value ="2"/>
+          <Button onClick={() => this.handleInsertNumber("3")} value ="3" />
+          <Button onClick ={() => this.handleInsertOperation("+")} value ="+" />
+          <Button value ="" />
+          <Button onClick={() => this.handleInsertNumber("0")} value ="0" />
+          <Button value ="" />
+          <Button onClick={this.handleEndOperation.bind(this)} value ="="/>
+        </div>
       </div>
-    </div>
-  );
-};
+    );
+  }
+  
+}
 
 export default App;
